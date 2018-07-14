a16840d518e21455bb4b7bd9922d03aead9bfc3f
CoolFullstackDev <topace1029@hotmail.com>
2018-07-13 23:05:25 -0700

    initial-commit

---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title':'Article One',
+            'body':'lorem ipsum dolor sit amet',
+            'author':'Brad Traversy',
+            'create_date':'04-25-2017'
+        },
+        {
+            'id': 2,
+            'title':'Article two',
+            'body':'lorem ipsum dolor sit amet',
+            'author':'Brad Traversy',
+            'create_date':'04-25-2017'
+        },
+        {
+            'id': 3,
+            'title':'Article three',
+            'body':'lorem ipsum dolor sit amet',
+            'author':'Brad Traversy',
+            'create_date':'04-25-2017'
+        }
+    ]
+
+    return articles
